3a98c1f631899dca5863e7cd9fbaea736a565155
salimbraksa <salim.braksa@gmail.com>
2015-12-10 18:31:56 +0000

    Initial Commit

---
+++ SBSlideMenu/SBSlideMenu.h
@@ -0,0 +1,19 @@
+//
+//  SBSlideMenu.h
+//  SBSlideMenu
+//
+//  Created by Salim Ive on 12/10/15.
+//  Copyright © 2015 Braksa Salim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SBSlideMenu.
+FOUNDATION_EXPORT double SBSlideMenuVersionNumber;
+
+//! Project version string for SBSlideMenu.
+FOUNDATION_EXPORT const unsigned char SBSlideMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBSlideMenu/PublicHeader.h>
+
+
